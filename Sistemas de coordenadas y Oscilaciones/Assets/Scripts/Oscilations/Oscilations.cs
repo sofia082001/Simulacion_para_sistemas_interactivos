a550c50f46be7017f7350d9e439a75456ed8f20a
Sofia <68034198+sofia082001@users.noreply.github.com>
2022-10-04 01:13:09 -0500

    correcion en oscilations

--- Sistemas de coordenadas y Oscilaciones/Assets/Scripts/Oscilations/Oscilations.cs
+++ Sistemas de coordenadas y Oscilaciones/Assets/Scripts/Oscilations/Oscilations.cs
@@ -21,7 +21,7 @@ public class Oscilations : MonoBehaviour
         //rhu = Mathf.Sin(2f * Mathf.Sin (7f * Time.time) + Mathf.Sin(4f*Time.time) + Mathf.Sin(2f * Time.time) + Mathf.Sin(3f * Time.time)); 
         //transform.position = initialPosition + Vector3.right * rhu * alcance; //MOVIMIENTOS RANDOM
        
-        //transform.position = initialPosition + Vector3.right * Mathf.Sin(2f * Mathf.PI * (Time.time / period)) * alcance; //MOVIMIENTO HORIZONTAL CON FORMULA del periodo
+        transform.position = initialPosition + Vector3.right * Mathf.Sin(2f * Mathf.PI * (Time.time / period)) * alcance; //MOVIMIENTO HORIZONTAL CON FORMULA del periodo
         
         //transform.position = initialPosition + (Vector3.right + Vector3.up) * Mathf.Sin(Time.time * period) * alcance;    //MOVIMIENTO DIAGONAL
         
